985ac84053cb4da2703b23a07e40f16504d3a7d5
James Howard <james@plumpdigital.co.uk>
2014-05-30 13:38:55 +0100

    Fix indentation.

--- app/index.js
+++ app/index.js
@@ -7,31 +7,31 @@ var chalk = require('chalk');
 
 //Bower inuit package options (prepended with inuit-)
 var inuitModules = [
-  'defaults',
-  'functions',
-  'mixins',
-  'normalize',
-  'reset',
-  'box-sizing',
-  'shared',
-  'clearfix',
-  'page',
-  'headings',
-  'paragraphs',
-  'lists',
-  'images',
-  'tables',
-  'layout',
-  'media',
-  'flag',
-  'box',
-  'tabs',
-  'buttons',
-  'bare-list',
-  'block-list',
-  'ui-list',
-  'spacing',
-  'widths'
+	'defaults',
+	'functions',
+	'mixins',
+	'normalize',
+	'reset',
+	'box-sizing',
+	'shared',
+	'clearfix',
+	'page',
+	'headings',
+	'paragraphs',
+	'lists',
+	'images',
+	'tables',
+	'layout',
+	'media',
+	'flag',
+	'box',
+	'tabs',
+	'buttons',
+	'bare-list',
+	'block-list',
+	'ui-list',
+	'spacing',
+	'widths'
 ];
 
 //Bower plump module options.
@@ -45,92 +45,89 @@ var plumpModules = [
  */
 var PlumpGenerator = yeoman.generators.Base.extend({
 
-  init: function () {
-    this.pkg = require('../package.json');
+	init: function () {
+		this.pkg = require('../package.json');
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });
+		this.on('end', function () {
+			this.log(chalk.magenta('I\'m all done. You now need to run npm install / bower install.'));
+		});
+	},
+
+	/**
+	 *    Prompts for project configuration.
+	 *
+	 * 1. Create asynchronous callback to pause execution
+	 *    until all the prompts are completed.
+	 */
+	askFor: function () {
+		var done = this.async(); /* [1] */
+
+		// have Yeoman greet the user
+		this.log(this.yeoman);
+
+		// replace it with a short and sweet description of your generator
+		this.log(chalk.magenta('You\'re using the Plump web generator.'));
+
+		var prompts = [{
+			type: 'checkbox',
+			name: 'inuitModules',
+			message: 'Which inuit modules do you require?',
+			choices : this._getModuleChoices(inuitModules)
+		}];
+
+		this.prompt(prompts, function (props) {
+			this.inuitModules = props.inuitModules;
+			done();
+		}.bind(this));
   },
 
-  /**
-   *    Prompts for project configuration.
-   *
-   * 1. Create asynchronous callback to pause execution
-   *    until all the prompts are completed.
-   */
-  askFor: function () {
-    var done = this.async(); /* [1] */
+	/**
+	 *    Create the app files.
+	 *
+	 * 1. Primary source directory is /src
+	 * 2. Create NPM and Bower config. Using template rather than
+	 *    copy runs the template through the Lo-Dash parser.
+	 */
+	app: function () {
+		this.mkdir('src'); /* [1] */
 
-    // have Yeoman greet the user
-    this.log(this.yeoman);
+		/* [2] */
+		this.template('_package.json', 'package.json');
+		this.template('_bower.json', 'bower.json');
 
-    // replace it with a short and sweet description of your generator
-    this.log(chalk.magenta('You\'re using the Plump web generator.'));
+		/* [3] */
+		this.mkdir('src/templates/partials');
+		this.mkdir('src/templates/layout');
+		this.mkdir('src/images');
+		this.mkdir('src/styles');
+		this.mkdir('src/scripts');
+	},
 
-    var prompts = [{
-      type: 'checkbox',
-      name: 'inuitModules',
-      message: 'Which inuit modules do you require?',
-      choices : this._getModuleChoices(inuitModules)
-    }];
+	/**
+	 *    Create additional app configuration files.
+	 *
+	 * 1. Editor config (http://editorconfig.org/)
+	 * 2. JSHint config (http://www.jshint.com/docs/)
+	 * 3. Basic .gitignore to ignore NPM and Bower components.
+	 */
+	 projectfiles: function () {
+		this.copy('editorconfig', '.editorconfig'); /* [1] */
+		this.copy('jshintrc', '.jshintrc'); /* [2] */
+		this.copy('gitignore', '.gitignore'); /* [3] */
 
-    this.prompt(prompts, function (props) {
-      this.inuitModules = props.inuitModules;
+		this.copy('gulpfile.js', 'gulpfile.js');
+	},
 
-      done();
-    }.bind(this));
-  },
-
-  /**
-   *    Create the app files.
-   *
-   * 1. Primary source directory is /src
-   * 2. Create NPM and Bower config. Using template rather than
-   *    copy runs the template through the Lo-Dash parser.
-   */
-  app: function () {
-    this.mkdir('src'); /* [1] */
-
-    /* [2] */
-    this.template('_package.json', 'package.json');
-    this.template('_bower.json', 'bower.json');
-
-    /* [3] */
-    this.mkdir('src/templates/partials');
-    this.mkdir('src/templates/layout');
-    this.mkdir('src/images');
-    this.mkdir('src/styles');
-    this.mkdir('src/scripts');
-  },
-
-  /**
-   *    Create additional app configuration files.
-   *
-   * 1. Editor config (http://editorconfig.org/)
-   * 2. JSHint config (http://www.jshint.com/docs/)
-   * 3. Basic .gitignore to ignore NPM and Bower components.
-   */ 
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig'); /* [1] */
-    this.copy('jshintrc', '.jshintrc'); /* [2] */
-    this.copy('gitignore', '.gitignore'); /* [3] */
-
-    this.copy('gulpfile.js', 'gulpfile.js');
-  },
-
-  /**
-   *    Helper to create a choices array for checkbox prompts.
-   */
-  _getModuleChoices: function(modules) {
-    var choices = [];
-    for (var i = 0; i < modules.length; i++) {
-      choices[i] = { name : modules[i] };
-    }
-    return choices;
-  }
+	/**
+	 *    Helper to create a choices array for checkbox prompts.
+	 */
+	_getModuleChoices: function(modules) {
+		var choices = [];
+		for (var i = 0; i < modules.length; i++) {
+			choices[i] = { name : modules[i] };
+		}
+		return choices;
+	}
 
 });
 
