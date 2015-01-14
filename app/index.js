cfdf31c2e5bbbe843cf4432869f64e8e6fb03d40
James Martin-Davies <jamesmd@plumpdigital.co.uk>
2015-01-14 11:19:11 +0000

    Add auto-dependency installation.

--- app/index.js
+++ app/index.js
@@ -94,10 +94,7 @@ var PlumpGenerator = yeoman.generators.Base.extend({
 		this.pkg = require('../package.json');
 
 		this.on('end', function () {
-			this.log(chalk.magenta.bold('I\'m all done. You now need to run ') +
-					 chalk.green.bold('npm install ') +
-					 chalk.magenta.bold('& ') +
-					 chalk.green.bold('bower install'));
+			this.installDependencies();
 		});
 	},
 
